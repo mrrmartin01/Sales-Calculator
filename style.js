127ba0dfd2758eeddac49ffed085e426f797919d
mrrmartin01 <Theophilusmartin123@gmail.com>
2023-12-18 14:21:04 +0000

    Intial commit

---
+++ style.js
@@ -0,0 +1,7 @@
+function openPage(page) {
+    if(page === 'refresh') {
+        window.location.href = 'index.html';
+    } else if (page === 'login'){
+        window.location.href ='login.html';
+    }
+};
