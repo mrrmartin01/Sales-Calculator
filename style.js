32e5c8e2226f1c53966430697312187948159694
mrrmartin01 <Theophilusmartin123@gmail.com>
2023-12-23 17:54:56 +0000

    FInal touches to the login UI

--- style.js
+++ style.js
@@ -4,4 +4,20 @@ function openPage(page) {
     } else if (page === 'login'){
         window.location.href ='login.html';
     }
+    else if (page === 'notes'){
+        window.location.href ='note.html';
+    }
 };
+
+// function validateEmail() {
+//     const email = document.getElementById('emailSide').value;
+//     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//     if (!emailPattern.test(email)) {
+//         alert('Please enter a valid email address.');
+//         return false; // Prevent form submission
+//     }
+
+//     // If the email is valid, redirect to notes.html
+//     window.location.href = 'notes.html';
+// }
